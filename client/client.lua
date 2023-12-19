a2f3d2a796d5e5e5f204e37dc2f0a7d9263e9f3f
Hur3NTv <simone.ferlich@gmail.com>
2023-12-19 02:58:17 +0100

    LMD2

---
+++ client/client.lua
@@ -0,0 +1,63 @@
+CreatedThread(function()
+    while true do
+        Wait(500)
+
+        local playerId      = PlayerPedId()
+        local playerCoords  = GetEntityCoords(playerId)
+        local playerHeading = GetEntityHeading(playerId)
+
+        SendNUIMessage({
+            type    = 'position',
+            x       = playerCoords.x,
+            y       = playerCoords.y,
+            z       = playerCoords.z,
+            heading = playerHeading,
+        })
+    end
+end)
+
+CreatedThread(function()
+    while true do
+        Wait(1000)
+
+        SendNUIMessage({
+            type = 'ping',
+        })
+    end
+end)
+
+--
+-- NUI Callbacks
+--
+
+RegisterNUICallbacks('pong', function(data, cb)
+    print('Got pong, foo value is', data.foo)
+
+    cb({
+        acceptedPong = true,
+    })
+end)
+
+RegisterNUICallbacks('releaseFocus', function(data, cb)
+    cb({})
+
+    SetNuiFocus(false, false)
+end)
+
+RegisterNUICallbacks('teleport', function(data, cb)
+    cb({})
+
+    print(data.x, data.y, data.z)
+    
+    SetEntityCoords(PlayerPedId(), tonunber(data.x), tonunber(data.y), tonunber(data.z))          -- SetNuiFocus(PlayerPedId(), data.x, data.y, data.z)
+end)
+
+--
+-- Key Bidings
+--
+
+RegisterCommand('+opentelepèort', function()
+    SetNuiFocus(true, true)
+end)
+
+RegisterKeyMapping('+openteleporter', 'Open Teleporter', 'keyboard', 'F5')
---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game       'gta5'
+lua54      'yes'
+
+author 'LMD22 Dev'
+description 'Simple Hud Teleport'
+version '1.0.0'
+
+--
+-- Client
+--
+
+client_scripts {
+    'client/client.lua',
+}
+
+--
+-- NUI
+--
+
+ui_page 'nui/nui.html'
+
+files {
+    'mui/nui.html',
+    'nui/style.css',
+    'nui/script.js',
+    'nui/images/*',
+}
